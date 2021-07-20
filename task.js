3dffe703bdf599e15115bb0d72adef74590edb8f
Tushar <tusharruhela80@gmail.com>
2021-07-20 05:59:23 -0700

    second commit

---
+++ task.js
@@ -0,0 +1,21 @@
+function setNewImage() {
+    document.getElementById("img1").src =
+      "https://innolabz.in/static/img/ps_ho.png";
+  }
+  function setoldimage(){
+      document.getElementById('img1').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+  }
+  function setsecondImage() {
+    document.getElementById("img2").src =
+      "https://innolabz.in/static/img/ps_ho.png";
+  }
+  function setold2image(){
+      document.getElementById('img2').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+  }
+  function setthirdImage() {
+    document.getElementById("img3").src =
+      "https://innolabz.in/static/img/ps_ho.png";
+  }
+  function setold3image(){
+      document.getElementById('img3').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+  }
