61dd442a30239eb49d70908eca4659c19984200d
Tushar <tusharruhela80@gmail.com>
2021-08-05 23:09:43 -0700

    first commit

--- task.js
+++ task.js
@@ -1,21 +1,21 @@
 function setNewImage() {
     document.getElementById("img1").src =
-      "https://innolabz.in/static/img/ps_ho.png";
+      "images/new-image.png";
   }
   function setoldimage(){
-      document.getElementById('img1').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+      document.getElementById('img1').src='images/power2startup-logo.png';
   }
   function setsecondImage() {
     document.getElementById("img2").src =
-      "https://innolabz.in/static/img/ps_ho.png";
+      "images/new-image.png";
   }
   function setold2image(){
-      document.getElementById('img2').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+      document.getElementById('img2').src="images/power2startup-logo.png";
   }
   function setthirdImage() {
     document.getElementById("img3").src =
-      "https://innolabz.in/static/img/ps_ho.png";
+      "images/new-image.png";
   }
   function setold3image(){
-      document.getElementById('img3').src="https://inno-labz.com/upload/program/power2startup-logo.png";
+      document.getElementById('img3').src="images/power2startup-logo.png";
   }
